47a3a8e7cf634a09d5c39db20add6c948576cc8d
Scott Bremmer <bremmerscottw@gmail.com>
2019-05-06 11:24:01 -0700

    You can now go places Yay!

--- Assets/Scripts/PlayerRaycast.cs
+++ Assets/Scripts/PlayerRaycast.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class PlayerRaycast : MonoBehaviour
 {
@@ -8,8 +9,13 @@ public class PlayerRaycast : MonoBehaviour
     float d, u, l, r;
     Vector2 origin;
     Vector2 nullPoint = new Vector2(0, 0);
+    Vector2 hitPoint;
+    const float rayDist = 0.9f;
+    const float offset = 0.25f;
 
     public LayerMask layerMask;
+    public GameObject doorPanel;
+    public Text doorText;
 
     void Start()
     {
@@ -25,25 +31,94 @@ public class PlayerRaycast : MonoBehaviour
     {
         origin = transform.position;
 
-        rayD = Physics2D.Raycast(origin + new Vector2(0, -.5f), Vector2.down, 5f, layerMask);
-        rayU = Physics2D.Raycast(origin + new Vector2(0, .5f), -Vector2.down, 5f, layerMask);
-        rayL = Physics2D.Raycast(origin + new Vector2(-.5f, 0), Vector2.left, 5f, layerMask);
-        rayR = Physics2D.Raycast(origin + new Vector2(.5f, 0), -Vector2.left, 5f, layerMask);
+        rayD = Physics2D.Raycast(origin + new Vector2(0, -offset), Vector2.down, rayDist, layerMask);
+        rayU = Physics2D.Raycast(origin + new Vector2(0, offset), -Vector2.down, rayDist, layerMask);
+        rayL = Physics2D.Raycast(origin + new Vector2(-offset, 0), Vector2.left, rayDist, layerMask);
+        rayR = Physics2D.Raycast(origin + new Vector2(offset, 0), -Vector2.left, rayDist, layerMask);
 
         d = rayD.distance;
         u = rayU.distance;
         l = rayL.distance;
         r = rayR.distance;
 
-        Debug.DrawRay(origin + new Vector2(0, -.5f), Vector2.down * 5f, Color.red);
-        Debug.DrawRay(origin + new Vector2(0,.5f), -Vector2.down * 5f, Color.cyan);
-        Debug.DrawRay(origin + new Vector2(-.5f, 0), Vector2.left * 5f, Color.cyan);
-        Debug.DrawRay(origin + new Vector2(.5f, 0), -Vector2.left * 5f, Color.cyan);
+        Debug.DrawRay(origin + new Vector2(0, -offset), Vector2.down * rayDist, Color.red);
+        Debug.DrawRay(origin + new Vector2(0, offset), -Vector2.down * rayDist, Color.cyan);
+        Debug.DrawRay(origin + new Vector2(-offset, 0), Vector2.left * rayDist, Color.cyan);
+        Debug.DrawRay(origin + new Vector2(offset, 0), -Vector2.left * rayDist, Color.cyan);
 
-        if (rayD.point == nullPoint)
-            Debug.Log("Null");
+        doorCheck();
+        doorEnter();
+
+        if(Vector3.Distance(origin, hitPoint) <= rayDist)
+        {
+            doorPanel.SetActive(true);
+            doorText.GetComponent<Text>().text = "Q";
+        }
         else
-            Debug.Log(d);
+        {
+            doorPanel.SetActive(false);
+        }
 
+        doorEntry();
+
+    }
+
+    void doorCheck()
+    {
+        if (rayD.point == nullPoint)
+            d = -1;
+        else
+            d = rayD.distance;
+        /**/
+        if (rayU.point == nullPoint)
+            u = -1;
+        else
+            u = rayU.distance;
+        /**/
+        if (rayL.point == nullPoint)
+            l = -1;
+        else
+            l = rayL.distance;
+        /**/
+        if (rayR.point == nullPoint)
+            r = -1;
+        else
+            r = rayR.distance;
+    }
+
+    void doorEnter()
+    {
+        if (d > u && d > l && d > r)
+            hitPoint = rayD.point;
+        else if (u > d && u > l && u > r)
+            hitPoint = rayU.point;
+        else if (l > r && l > u && l > d)
+            hitPoint = rayL.point;
+        else if (r > d && r > u && r > l)
+            hitPoint = rayR.point;
+        else if (d == -1 && u == -1 && l == -1 && r == -1)
+            hitPoint = new Vector2(0, 0);
+
+        hitPoint.x = Mathf.FloorToInt(hitPoint.x);
+        hitPoint.y = Mathf.FloorToInt(hitPoint.y);
+
+        Debug.Log(hitPoint);
+    }
+
+    void doorEntry()
+    {
+        if (Input.GetKey(KeyCode.Q) == true && doorPanel.activeInHierarchy) {
+            Debug.Log("oof");
+            if (hitPoint.x == 46 && hitPoint.y == -30 /* && SceneManager.GetActiveScene().name == "2D Liberyville"*/)
+            {
+                SceneManager.LoadScene("Tom Inside");
+                Debug.Log("True");
+            }
+            if (hitPoint.x == 44 && hitPoint.y == -28 /* && SceneManager.GetActiveScene().name == "2D Liberyville"*/)
+            {
+                SceneManager.LoadScene("2D Libertyville");
+                Debug.Log("True");
+            }
+        }
     }
 }
--- Assets/Scripts/TomProx2D.cs
+++ Assets/Scripts/TomProx2D.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TomProx2D : MonoBehaviour
 {
     public GameObject chatPanel;
+    public Text chatText;
     public GameObject tom;
 
     Vector2 tomPos;
@@ -11,13 +13,15 @@ public class TomProx2D : MonoBehaviour
 
     void Update()
     {
+        chatPanel.SetActive(false);
+
         playerPos = GetComponent<Transform>().position;
         tomPos = tom.GetComponent<Transform>().position;
 
         if (Vector3.Distance(tomPos, playerPos) <= 1.5f) //forms a line segment using the two points
         {
             chatPanel.SetActive(true); //shows the 'E' panel
-
+            chatText.GetComponent<Text>().text = "E"; //Changes the text to E
         }
         else
         {
