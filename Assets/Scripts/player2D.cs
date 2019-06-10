1e6d55c40d3abf4d30de65611ba85674fca73a10
Nathaniel Nelson <nnelson5302@gmail.com>
2019-06-10 10:03:00 -0700

    Discarded Moonwalking Feature

--- Assets/Scripts/player2D.cs
+++ Assets/Scripts/player2D.cs
@@ -34,13 +34,29 @@ public class player2D : MonoBehaviour
                 if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
                 {
                     MildredAnimation.SetFloat("speed", speed);
+                    if(Input.GetAxis("Horizontal") < 0)
+                    {
+                        MildredAnimation.SetInteger("direction", 4);
+                    }
+                    else if (Input.GetAxis("Horizontal") > 0)
+                    {
+                        MildredAnimation.SetInteger("direction", 3);
+                    }
+                    else if (Input.GetAxis("Vertical") < 0)
+                    {
+                        MildredAnimation.SetInteger("direction", 1);
+                    }
+                    else if (Input.GetAxis("Vertical") > 0)
+                    {
+                        MildredAnimation.SetInteger("direction", 2);
+                    }
                 }
                 else
                 {
                     MildredAnimation.SetFloat("speed", 0);
                 }
             }
-
+            /*
             if (Input.GetKeyDown("down") || Input.GetKeyDown("s"))
             {
                 MildredAnimation.SetInteger("direction", 1);
@@ -61,6 +77,96 @@ public class player2D : MonoBehaviour
                 MildredAnimation.SetInteger("direction", 3);
             }
 
+
+            if (Input.GetKeyUp("down") || Input.GetKeyUp("s"))
+            {
+                if (Input.GetKeyDown("down") || Input.GetKeyDown("s"))
+                {
+                    MildredAnimation.SetInteger("direction", 1);
+                }
+                else if (Input.GetKeyDown("up") || Input.GetKeyDown("w"))
+                {
+                    Debug.Log("Setting direction to 2");
+                    MildredAnimation.SetInteger("direction", 2);
+                }
+                else if (Input.GetKeyDown("left") || Input.GetKeyDown("a"))
+                {
+                    Debug.Log("Setting direction to 3");
+                    MildredAnimation.SetInteger("direction", 4);
+                }
+                else if (Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+                {
+                    Debug.Log("Setting direction to 4");
+                    MildredAnimation.SetInteger("direction", 3);
+                }
+            }
+            else if (Input.GetKeyUp("up") || Input.GetKeyUp("w"))
+            {
+                if (Input.GetKeyDown("down") || Input.GetKeyDown("s"))
+                {
+                    MildredAnimation.SetInteger("direction", 1);
+                }
+                else if (Input.GetKeyDown("up") || Input.GetKeyDown("w"))
+                {
+                    Debug.Log("Setting direction to 2");
+                    MildredAnimation.SetInteger("direction", 2);
+                }
+                else if (Input.GetKeyDown("left") || Input.GetKeyDown("a"))
+                {
+                    Debug.Log("Setting direction to 3");
+                    MildredAnimation.SetInteger("direction", 4);
+                }
+                else if (Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+                {
+                    Debug.Log("Setting direction to 4");
+                    MildredAnimation.SetInteger("direction", 3);
+                }
+            }
+            else if (Input.GetKeyUp("left") || Input.GetKeyUp("a"))
+            {
+                if (Input.GetKeyDown("down") || Input.GetKeyDown("s"))
+                {
+                    MildredAnimation.SetInteger("direction", 1);
+                }
+                else if (Input.GetKeyDown("up") || Input.GetKeyDown("w"))
+                {
+                    Debug.Log("Setting direction to 2");
+                    MildredAnimation.SetInteger("direction", 2);
+                }
+                else if (Input.GetKeyDown("left") || Input.GetKeyDown("a"))
+                {
+                    Debug.Log("Setting direction to 3");
+                    MildredAnimation.SetInteger("direction", 4);
+                }
+                else if (Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+                {
+                    Debug.Log("Setting direction to 4");
+                    MildredAnimation.SetInteger("direction", 3);
+                }
+            }
+            else if (Input.GetKeyUp("right") || Input.GetKeyUp("d"))
+            {
+                if (Input.GetKeyDown("down") || Input.GetKeyDown("s"))
+                {
+                    MildredAnimation.SetInteger("direction", 1);
+                }
+                else if (Input.GetKeyDown("up") || Input.GetKeyDown("w"))
+                {
+                    Debug.Log("Setting direction to 2");
+                    MildredAnimation.SetInteger("direction", 2);
+                }
+                else if (Input.GetKeyDown("left") || Input.GetKeyDown("a"))
+                {
+                    Debug.Log("Setting direction to 3");
+                    MildredAnimation.SetInteger("direction", 4);
+                }
+                else if (Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+                {
+                    Debug.Log("Setting direction to 4");
+                    MildredAnimation.SetInteger("direction", 3);
+                }
+            }
+            */
             transform.GetComponent<Rigidbody2D>().velocity = movement; //assigns the input variable to the gameObject as a velocity
 
         }
